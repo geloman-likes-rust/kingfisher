544a40c845d412e4120480611875ddd74901f668
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-16 23:21:26 +0800

    Create User Model

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from flask_login import UserMixin
+from sqlalchemy.orm import Mapped, mapped_column
+from app.shared.orm import db
+
+
+@dataclass
+class User(db.Model, UserMixin):
+
+    __tablename__ = "accounts"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
+    permission: Mapped[str] = mapped_column(default="read-only")
+    role: Mapped[str] = mapped_column(default="user")
+    is_online: Mapped[bool] = mapped_column(default=False)
