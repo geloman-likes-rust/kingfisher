768a2b3f7fb03b53718e255730acdf9c8abb24cd
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-17 00:25:26 +0800

    Update entrypoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from app.shared.orm import db
+from app.extensions.orm import orm
+from app.extensions.cors import cors
+from app.extensions.login import login
+from app.extensions.hasher import hasher
+from app.routes.accounts import accounts
+from app.routes.authentication import authentication
 
 app = Flask(__name__)
+app.config.from_prefixed_env()
 
-@app.get('/')
-def hello():
-    return "Hello from Singapore!"
+app.register_blueprint(orm)
+app.register_blueprint(cors)
+app.register_blueprint(login)
+app.register_blueprint(hasher)
+app.register_blueprint(accounts)
+app.register_blueprint(authentication)
+
+with app.app_context():
+    db.create_all()
