28d921cf199571234964fa2f8a361a7c769e9c34
geloman <geloman-likes-rust@users.noreply.github.com>
2024-04-21 18:04:59 +0800

    Use JWT for authentication

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, request
-from app.shared.hasher import bcrypt
+from app.shared.orm import db
 from app.models.user import User
+from app.shared.hasher import bcrypt
+from app.shared.credential import send_jwt
+from flask import Blueprint, Response, request
+from app.decorators.authorization import jwt_required
 
 
 authentication = Blueprint("authentication", __name__, url_prefix="/api/v1")
@@ -23,14 +26,34 @@ def login():
 
     password_did_match = bcrypt.check_password_hash(user.password, password)
     if password_did_match:
-
-        # TODO: send both access token and refresh token as httpOnly cookie
-
-        return "", 204  # 204 - NO CONTENT
+        return send_jwt(user)
 
     return "", 401  # 401 - UNAUTHORIZED
 
 
 @authentication.get("/logout")
-def logout():
-    return "", 204  # 204 - NO CONTENT
+@jwt_required
+def logout(jwt_payload):
+    if jwt_payload is None:
+        return Response(status=401)
+
+    username = jwt_payload.get("username")
+
+    if username is None:
+        return Response(status=401)
+
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return Response(status=401)
+
+    # Update online status of currently logged in user
+    user.token = ""
+    user.is_online = False
+    db.session.commit()
+
+    # Delete jwt tokens
+    response = Response(status=204)
+    response.delete_cookie("access_token")
+    response.delete_cookie("refresh_token")
+
+    return response
