8ee49b1d2219793a05429ef02439e9ef250145a7
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-21 00:41:07 +0800

    Add redis environments

--- app/shared/environments.py
+++ app/shared/environments.py
@@ -4,5 +4,10 @@ pg_host = getenv("PGHOST")
 pg_user = getenv("PGUSER")
 pg_password = getenv("PGPASSWORD")
 pg_database = getenv("PGDATABASE")
+
+redis_host = getenv("REDIS_HOST")
+redis_user = getenv("REDIS_USER")
+redis_password = getenv("REDIS_PASSWORD")
+
 jwt_secret = getenv("JWT_SECRET")
 allowed_origin = getenv("ALLOWED_ORIGIN")
