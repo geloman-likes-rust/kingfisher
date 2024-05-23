85f96bf63234e47bce6fc9c4c532c858611445ef
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-24 06:20:21 +0800

    Create function for changing password

--- app/queries/accounts.py
+++ app/queries/accounts.py
@@ -108,3 +108,27 @@ def get_account(username: str) -> Dict[str, Any] | None:
             release_connection(connection)
     except:
         return None
+
+
+def change_password(username: str, old_pass: str, new_pass: str) -> bool:
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        try:
+            query = """
+                UPDATE accounts
+                SET password = %s
+                WHERE username = %s
+                AND password = %s
+            """
+            cursor.execute(query, (new_pass, username, old_pass))
+            connection.commit()
+
+            change_password_successful: bool = cursor.rowcount == 1
+            return change_password_successful
+
+        finally:
+            cursor.close()
+            release_connection(connection)
+    except:
+        return False
