15cbedfe7f46f4096195d797f8858155c822effe
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-24 06:21:41 +0800

    Create function for changing account permission

--- app/queries/accounts.py
+++ app/queries/accounts.py
@@ -157,3 +157,27 @@ def change_role(username: str, role: str) -> bool:
             release_connection(connection)
     except:
         return False
+
+
+def change_permission(username: str, permission: str) -> bool:
+    if permission not in ("read-write", "read-only"):
+        return False
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        try:
+            query = """
+                UPDATE accounts
+                SET permission = %s
+                WHERE username = %s
+            """
+            cursor.execute(query, (permission, username))
+            connection.commit()
+
+            change_permission_successful: bool = cursor.rowcount == 1
+            return change_permission_successful
+        finally:
+            cursor.close()
+            release_connection(connection)
+    except:
+        return False
