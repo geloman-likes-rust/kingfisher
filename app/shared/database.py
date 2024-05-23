ae83498b002955c0f988fcf7aee58f25ba9576b6
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-24 03:55:31 +0800

    Create db connection pool

---
+++ app/shared/database.py
@@ -0,0 +1,19 @@
+from psycopg2.pool import SimpleConnectionPool
+from app.shared.environments import pg_host, pg_user, pg_password, pg_database
+
+connection_pool = SimpleConnectionPool(
+    minconn=1,
+    maxconn=4,
+    host=pg_host,
+    user=pg_user,
+    dbname=pg_database,
+    password=pg_password,
+)
+
+
+def get_connection():
+    return connection_pool.getconn()
+
+
+def release_connection(connection):
+    connection_pool.putconn(connection)
