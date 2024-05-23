051288356312731b1cff502faec9336b9ba9e420
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-24 03:50:07 +0800

    Remove from_prefixed_env

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from app.routes.authentication import authentication
 from app.queries.accounts import create_super_admin
 
 app = Flask(__name__)
-app.config.from_prefixed_env()
 
 app.register_blueprint(cors)
 app.register_blueprint(hasher)
