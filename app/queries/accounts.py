aa3bf1e822ef8bad9dbd6a7f9a48e731a9f3e954
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-24 06:20:58 +0800

    Create function for changing account role

--- app/queries/accounts.py
+++ app/queries/accounts.py
@@ -132,3 +132,28 @@ def change_password(username: str, old_pass: str, new_pass: str) -> bool:
             release_connection(connection)
     except:
         return False
+
+
+def change_role(username: str, role: str) -> bool:
+    if role not in ("admin", "user"):
+        return False
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        try:
+            query = """
+                UPDATE accounts
+                SET role = %s
+                WHERE username = %s
+            """
+            cursor.execute(query, (role, username))
+            connection.commit()
+
+            change_role_successful: bool = cursor.rowcount == 1
+            return change_role_successful
+
+        finally:
+            cursor.close()
+            release_connection(connection)
+    except:
+        return False
