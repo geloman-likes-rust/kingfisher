2851807a982d5d9469c1cbc88f2c2253c0134b8a
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-26 05:31:04 +0800

    Update columns from delete_individual function

--- app/queries/delete_individual.py
+++ app/queries/delete_individual.py
@@ -1,18 +1,21 @@
 from app.shared.database import get_connection, release_connection
 
 
-def delete_individual(name: str, position: str, company: str) -> bool:
+def delete_individual(
+    firstname: str, lastname: str, position: str, company: str
+) -> bool:
     try:
         connection = get_connection()
         cursor = connection.cursor()
         try:
             query = """
                 DELETE FROM individuals
-                WHERE name = %s
+                WHERE firstname = %s
+                AND lastname = %s
                 AND position_id = (SELECT id FROM positions WHERE name = %s)
                 AND company_id = (SELECT id FROM companies WHERE name = %s)
             """
-            cursor.execute(query, (name, position, company))
+            cursor.execute(query, (firstname, lastname, position, company))
             connection.commit()
 
             deletion_success: bool = cursor.rowcount == 1
