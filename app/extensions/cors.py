9c9d07ccd6c6c3e369380d1af1aaf2848c9f38d0
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-26 09:52:13 +0800

    Update cors config

--- app/extensions/cors.py
+++ app/extensions/cors.py
@@ -7,10 +7,10 @@ cors = Blueprint("cors", __name__)
 
 @cors.record_once
 def on_load(state):
+    five_minutes = 300
     CORS(
         state.app,
-        max_age=300,
+        max_age=five_minutes,
         origins=allowed_origin or "http://localhost:5173",
-        supports_credentials=True,
         expose_headers=["Authorization"],
     )
