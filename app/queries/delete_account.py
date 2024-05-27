5c8bd9f7c946bae12e49ca8968638d1db82cc0a3
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-28 03:20:02 +0800

    Update delete account query

--- app/queries/delete_account.py
+++ app/queries/delete_account.py
@@ -1,7 +1,14 @@
+from typing import Literal
 from app.shared.database import get_connection, release_connection
 
 
-def delete_account(username: str) -> bool:
+def delete_account(
+    username: str | None = None,
+) -> Literal["NoUsername", "Success", "Failed", "DatabaseUnavailable"]:
+
+    if not username:
+        return "NoUsername"
+
     try:
         connection = get_connection()
         cursor = connection.cursor()
@@ -12,11 +19,14 @@ def delete_account(username: str) -> bool:
             """
             cursor.execute(query, (username,))
             connection.commit()
+
             deletion_success: bool = cursor.rowcount == 1
-            return deletion_success
+            if deletion_success:
+                return "Success"
+            return "Failed"
 
         finally:
             cursor.close()
             release_connection(connection)
     except:
-        return False
+        return "DatabaseUnavailable"
