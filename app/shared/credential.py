d7a1e8dddebfe617ba48f2fdea57867eca620dfe
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-29 08:34:48 +0800

    Create functions for creating and revoking refresh token

--- app/shared/credential.py
+++ app/shared/credential.py
@@ -1,55 +1,91 @@
-from os import getenv
 from jwt import encode
-from flask import Response
-from app.shared.orm import db
-from app.models.user import User
+from uuid import uuid4
+from typing import Dict
+from http import HTTPStatus
+from flask import Response, jsonify
+from app.shared.redis import redis_client
 from datetime import datetime, timedelta, UTC
+from app.shared.environments import jwt_secret
 
 
-def send_jwt(user: User) -> Response:
+def send_jwt(payload: Dict[str, str]) -> Response:
+    match jwt_secret:
+        case str():
+            access_token: str = encode(
+                payload={
+                    "exp": datetime.now(UTC) + timedelta(hours=1),
+                    "username": payload["username"],
+                    "permission": payload["permission"],
+                    "role": payload["role"],
+                },
+                key=jwt_secret,
+                algorithm="HS256",
+            )
 
-    jwt_secret = getenv("JWT_SECRET")
-    if jwt_secret is None:
-        return Response(status=500)  # 500 - INTERNAL SERVER ERROR
+            return jsonify({**payload, "access_token": access_token})
 
-    token = {
-        "exp": datetime.now(UTC) + timedelta(hours=1),
-        "username": user.username,
-        "permission": user.permission,
-        "role": user.role,
-    }
+        case None:
+            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    access_token = encode(token, jwt_secret, algorithm="HS256")
 
-    response = Response()
+def create_refresh_token(username: str, role="user", permission="read-only"):
 
-    sixty_days = 60 * 60 * 24 * 60  # seconds * minutes * hours * days
+    current_time = datetime.now(UTC)
+    token_expiration = current_time + timedelta(days=1)
 
-    response.set_cookie(
-        secure=True,
-        httponly=True,
-        samesite="None",
-        key="access_token",
-        value=access_token,
-        max_age=sixty_days,
+    jwt_id = str(uuid4())
+
+    refresh_token = encode(
+        payload={
+            "jti": jwt_id,
+            "exp": token_expiration,
+            "username": username,
+            "permission": permission,
+            "role": role,
+        },
+        key=jwt_secret,
+        algorithm="HS256",
     )
 
-    token["exp"] = datetime.now(UTC) + timedelta(days=60)
-    refresh_token = encode(token, jwt_secret, algorithm="HS256")
+    token_expiration_in_seconds = int(
+        token_expiration.timestamp() - current_time.timestamp()
+    )
 
-    # Update token and online status of user when logging in
-    user.is_online = True
-    user.token = refresh_token
-    db.session.commit()
-
-    response.set_cookie(
-        secure=True,
-        httponly=True,
-        samesite="None",
-        key="refresh_token",
+    redis_client.setex(
+        name=f"login-session:{username}:{jwt_id}",
         value=refresh_token,
-        max_age=sixty_days,
+        time=token_expiration_in_seconds,
     )
 
-    response.status = 204  # 204 - NO CONTENT
-    return response
+    return refresh_token
+
+
+def revoke_refresh_token(token_id: str, username: str):
+    deleted = redis_client.delete(f"login-session:{username}:{token_id}")
+    match deleted:
+        case int():
+            if deleted > 0:
+                return True
+            return False
+        case _:
+            return False
+
+
+def revoke_refresh_tokens(username: str) -> bool:
+    sessions = redis_client.keys(f"login-session:{username}:*")
+    match sessions:
+        case []:
+            return False
+
+        case list():
+            deleted = redis_client.delete(*sessions)
+            match deleted:
+                case int():
+                    if deleted > 0:
+                        return True
+                    return False
+                case _:
+                    return False
+
+        case _:
+            return False
