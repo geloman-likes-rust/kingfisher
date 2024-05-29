d67c54ceabefd3f07001f45752dc4a5291f68b25
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-29 08:36:18 +0800

    Update authentication routes

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -1,9 +1,12 @@
-from app.shared.orm import db
-from app.models.user import User
+from jwt import decode
+from typing import Dict
+from http import HTTPStatus
 from app.shared.hasher import bcrypt
-from app.shared.credential import send_jwt
 from flask import Blueprint, Response, request
+from app.shared.environments import jwt_secret
+from app.queries.get_account import get_account
 from app.decorators.authorization import jwt_required
+from app.shared.credential import send_jwt, revoke_refresh_token, create_refresh_token
 
 
 authentication = Blueprint("authentication", __name__, url_prefix="/api/v1")
@@ -11,49 +14,67 @@ authentication = Blueprint("authentication", __name__, url_prefix="/api/v1")
 
 @authentication.post("/login")
 def login():
-    payload = request.json
-    if payload is None:
-        return "", 400  # 400 - BAD REQUEST
+    payload: Dict[str, str] | None = request.json
+    if not payload:
+        return Response(status=HTTPStatus.BAD_REQUEST)
 
     username = payload.get("username")
     password = payload.get("password")
-    if username is None or password is None:
-        return "", 400  # 400 - BAD REQUEST
 
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        return "", 401  # 401 - UNAUTHORIZED
+    if not username or not password:
+        return Response(status=HTTPStatus.BAD_REQUEST)
 
-    password_did_match = bcrypt.check_password_hash(user.password, password)
-    if password_did_match:
-        return send_jwt(user)
+    account = get_account(username)
+    match account:
+        case dict():
+            password_match = bcrypt.check_password_hash(account["password"], password)
+            match password_match:
+                case True:
 
-    return "", 401  # 401 - UNAUTHORIZED
+                    username = account["username"]
+                    role = account["role"]
+                    permission = account["permission"]
+
+                    refresh_token = create_refresh_token(username, role, permission)
+
+                    return send_jwt(
+                        {
+                            "username": username,
+                            "role": role,
+                            "permission": permission,
+                            "refresh_token": refresh_token,
+                        }
+                    )
+
+                case False:
+                    return Response(status=HTTPStatus.UNAUTHORIZED)
+
+        case None:
+            return Response(status=HTTPStatus.UNAUTHORIZED)
 
 
 @authentication.get("/logout")
 @jwt_required
-def logout(jwt_payload):
-    if jwt_payload is None:
-        return Response(status=401)
+def logout(_):
+    payload: Dict[str, str] | None = request.json
+    match payload:
+        case dict():
+            refresh_token: str | None = payload.get("refresh_token")
+            match refresh_token:
+                case str():
+                    jwt_payload = decode(
+                        jwt=refresh_token, key=jwt_secret, algorithms=["HS256"]
+                    )
 
-    username = jwt_payload.get("username")
+                    token_id: str = jwt_payload["jti"]
+                    username: str = jwt_payload["username"]
 
-    if username is None:
-        return Response(status=401)
-
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        return Response(status=401)
-
-    # Update online status of currently logged in user
-    user.token = ""
-    user.is_online = False
-    db.session.commit()
-
-    # Delete jwt tokens
-    response = Response(status=204)
-    response.delete_cookie("access_token")
-    response.delete_cookie("refresh_token")
-
-    return response
+                    match revoke_refresh_token(token_id, username):
+                        case True:
+                            return Response(status=HTTPStatus.NO_CONTENT)
+                        case False:
+                            return Response(status=HTTPStatus.NOT_FOUND)
+                case None:
+                    return Response(status=HTTPStatus.BAD_REQUEST)
+        case None:
+            return Response(status=HTTPStatus.BAD_REQUEST)
