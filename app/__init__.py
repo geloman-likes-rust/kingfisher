810451798cdec5dfea9db1553331ab1de16a78d5
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-29 21:46:19 +0800

    Update entry point

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from app.extensions.cors import cors
 from app.extensions.hasher import hasher
+
+from app.routes.token import token
 from app.routes.accounts import accounts
 from app.routes.authentication import authentication
+
 from app.queries.create_super_admin import create_super_admin
 
 app = Flask(__name__)
 
+# FLASK EXTENSIONS
 app.register_blueprint(cors)
 app.register_blueprint(hasher)
+
+# API ROUTES
+app.register_blueprint(token)
 app.register_blueprint(accounts)
 app.register_blueprint(authentication)
 
