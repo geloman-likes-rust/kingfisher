f875aaa7bd4776e0dcb71669320ed37973dceb3e
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-29 19:56:38 +0800

    Update refresh token endpoint

--- app/routes/token.py
+++ app/routes/token.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Any, Dict
 from http import HTTPStatus
 from flask import Blueprint, Response, request
 from app.shared.environments import jwt_secret
@@ -19,58 +19,83 @@ def refresh_token():
             return Response(status=HTTPStatus.UNAUTHORIZED)
 
         case str():
-            try:
-                access_token: str = authorization.split(" ")[1]
-                jwt_payload = decode(access_token, jwt_secret, algorithms=["HS256"])
+            match jwt_secret:
+                case None:
+                    return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
-                payload = {
-                    "username": jwt_payload["username"],
-                    "permission": jwt_payload["permission"],
-                    "role": jwt_payload["role"],
-                }
+                case str():
 
-                return send_jwt(payload)
+                    # ACCESS TOKEN IS STILL VALID
+                    try:
+                        access_token: str = authorization.split(" ")[1]
 
-            except ExpiredSignatureError:
-                payload: Dict[str, str] | None = request.json
-                match payload:
-                    case dict():
-                        refresh_token = payload.get("refresh_token")
-                        match refresh_token:
-                            case str():
+                        jwt_payload: Dict[str, Any] = decode(
+                            access_token, jwt_secret, algorithms=["HS256"]
+                        )
+
+                        username: str = jwt_payload["username"]
+                        permission: str = jwt_payload["permission"]
+                        role: str = jwt_payload["role"]
+
+                        # SEND NEW ACCESS TOKEN
+                        return send_jwt(
+                            {
+                                "username": username,
+                                "permission": permission,
+                                "role": role,
+                            }
+                        )
+
+                    # ACCESS TOKEN HAS EXPIRED
+                    except ExpiredSignatureError:
+                        payload: Dict[str, str] | None = request.json
+                        match payload:
+                            case dict():
+
+                                refresh_token: str | None = payload.get("refresh_token")
+                                if not refresh_token:
+                                    return Response(status=HTTPStatus.UNAUTHORIZED)
+
                                 try:
-                                    jwt_payload = decode(
-                                        refresh_token, jwt_secret, algorithms=["HS256"]
+                                    jwt_payload: Dict[str, Any] = decode(
+                                        refresh_token,
+                                        jwt_secret,
+                                        algorithms=["HS256"],
                                     )
 
-                                    jwt_id = jwt_payload["jti"]
-                                    username = jwt_payload["username"]
+                                    jwt_id: str = jwt_payload["jti"]
+                                    username: str = jwt_payload["username"]
 
                                     match verify_refresh_token(jwt_id, username):
+                                        # VALID REFRESH TOKEN
                                         case True:
-                                            role = jwt_payload["role"]
-                                            permission = jwt_payload["permission"]
 
-                                            payload = {
-                                                "username": username,
-                                                "permission": permission,
-                                                "role": role,
-                                            }
+                                            role: str = jwt_payload["role"]
+                                            permission: str = jwt_payload["permission"]
 
-                                            return send_jwt(payload)
+                                            # SEND NEW ACCESS TOKEN
+                                            return send_jwt(
+                                                {
+                                                    "username": username,
+                                                    "permission": permission,
+                                                    "role": role,
+                                                }
+                                            )
 
+                                        # INVALID REFRESH TOKEN
                                         case False:
                                             return Response(
                                                 status=HTTPStatus.UNAUTHORIZED
                                             )
 
+                                # REFRESH TOKEN HAS EXPIRED OR INVALID REFRESH TOKEN
                                 except (ExpiredSignatureError, InvalidTokenError):
                                     return Response(status=HTTPStatus.UNAUTHORIZED)
 
+                            # REQUEST PAYLOAD IS NONE
                             case None:
                                 return Response(status=HTTPStatus.UNAUTHORIZED)
-                    case None:
-                        return Response(status=HTTPStatus.UNAUTHORIZED)
 
-            except InvalidTokenError:
-                return Response(status=HTTPStatus.UNAUTHORIZED)
+                    # INVALID ACCESS TOKEN
+                    except InvalidTokenError:
+                        return Response(status=HTTPStatus.UNAUTHORIZED)
