91bc62024d08860289b081823a687f02620a3402
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-31 04:19:13 +0800

    Cache the get_account query

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -5,6 +5,7 @@ from flask import Blueprint, Response, request
 from app.shared.environments import jwt_secret
 from app.queries.get_account import get_account
 from app.decorators.authorization import jwt_required
+from app.shared.cache import cache_response, get_cache
 from jwt import decode, ExpiredSignatureError, InvalidTokenError
 from app.shared.credential import send_jwt, revoke_refresh_token, create_refresh_token
 
@@ -24,16 +25,18 @@ def login():
     if not username or not password:
         return Response(status=HTTPStatus.BAD_REQUEST)
 
-    account = get_account(username)
-    match account:
+    endpoint = f"/login?username={username}"
+    cached_user = get_cache(endpoint)
+    match cached_user:
+        # CACHE HIT
         case dict():
-            password_match = bcrypt.check_password_hash(account["password"], password)
+            password_hash: str = cached_user["password"]
+            password_match: bool = bcrypt.check_password_hash(password_hash, password)
             match password_match:
                 case True:
-
-                    username = account["username"]
-                    role = account["role"]
-                    permission = account["permission"]
+                    username = cached_user["username"]
+                    role = cached_user["role"]
+                    permission = cached_user["permission"]
 
                     refresh_token = create_refresh_token(username, role, permission)
                     match refresh_token:
@@ -52,8 +55,48 @@ def login():
                 case False:
                     return Response(status=HTTPStatus.UNAUTHORIZED)
 
-        case None:
-            return Response(status=HTTPStatus.UNAUTHORIZED)
+        # CACHE MISS
+        case None | _:
+            account = get_account(username)
+            match account:
+                case dict():
+                    # 60 * 60 * 24 * 7 = 604800 (7 days in seconds)
+                    seven_days = 604800
+                    cache_response(endpoint, account, ttl=seven_days)
+
+                    password_hash: str = account["password"]
+                    password_match: bool = bcrypt.check_password_hash(
+                        password_hash, password
+                    )
+                    match password_match:
+                        case True:
+                            username = account["username"]
+                            role = account["role"]
+                            permission = account["permission"]
+
+                            refresh_token = create_refresh_token(
+                                username, role, permission
+                            )
+                            match refresh_token:
+                                case str():
+                                    return send_jwt(
+                                        {
+                                            "username": username,
+                                            "role": role,
+                                            "permission": permission,
+                                            "refresh_token": refresh_token,
+                                        }
+                                    )
+                                case None:
+                                    return Response(
+                                        status=HTTPStatus.INTERNAL_SERVER_ERROR
+                                    )
+
+                        case False:
+                            return Response(status=HTTPStatus.UNAUTHORIZED)
+
+                case None:
+                    return Response(status=HTTPStatus.UNAUTHORIZED)
 
 
 @authentication.post("/logout")
