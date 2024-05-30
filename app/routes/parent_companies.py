defca1a7349e6d760b6b9b98a1624623b8701dce
geloman <geloman-likes-rust@users.noreply.github.com>
2024-05-31 05:32:20 +0800

    Cache response for /{company}/parent-companies endpoint

--- app/routes/parent_companies.py
+++ app/routes/parent_companies.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 from flask import Blueprint, Response, jsonify
 from app.decorators.authorization import jwt_required
+from app.shared.cache import get_cache, cache_response
 
 
 parent_companies = Blueprint(
@@ -15,9 +16,19 @@ def get_parent_companies(_, company: str):
 
     company = company.replace("%20", " ").replace("+", " ")
 
-    parent_companies = get_parent_companies(company)
-    match parent_companies:
-        case list():
-            return jsonify(parent_companies)
+    endpoint = f"/companies/{company}/parent-companies"
+    cached = get_cache(endpoint)
+    match cached:
+        # CACHE HIT
+        case list() | dict():
+            return jsonify(cached)
+
+        # CACHE MISS
         case None:
-            return Response(status=HTTPStatus.NOT_FOUND)
+            parent_companies = get_parent_companies(company)
+            match parent_companies:
+                case list():
+                    cache_response(endpoint, parent_companies)
+                    return jsonify(parent_companies)
+                case None:
+                    return Response(status=HTTPStatus.NOT_FOUND)
