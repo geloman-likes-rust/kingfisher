49930ebf8d4d1bae8e991867e7ca7b500ab9cd85
geloman <geloman-likes-rust@users.noreply.github.com>
2024-06-01 09:07:48 +0800

    Update create_position query function

--- app/queries/create_position.py
+++ app/queries/create_position.py
@@ -1,7 +1,11 @@
+from typing import Literal
+from psycopg2 import Error, IntegrityError
 from app.shared.database import get_connection, release_connection
 
 
-def create_position(position: str) -> bool:
+def create_position(
+    position: str,
+) -> Literal["Success", "NotUnique", "DatabaseUnavailable"]:
     try:
         connection = get_connection()
         cursor = connection.cursor()
@@ -11,9 +15,14 @@ def create_position(position: str) -> bool:
             """
             cursor.execute(query, (position,))
             connection.commit()
-            return True
+            return "Success"
+
         finally:
             cursor.close()
             release_connection(connection)
-    except:
-        return False
+
+    except IntegrityError:
+        return "NotUnique"
+
+    except Error:
+        return "DatabaseUnavailable"
