3dfa80ba5ed629e51bd281d68a98390e13c39d3d
geloman <geloman-likes-rust@users.noreply.github.com>
2024-06-01 09:08:39 +0800

    Create endpoint for positions

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from app.extensions.hasher import hasher
 from app.routes.token import token
 from app.routes.accounts import accounts
 from app.routes.companies import companies
+from app.routes.positions import positions
 from app.routes.individuals import individuals
 from app.routes.authentication import authentication
 from app.routes.parent_companies import parent_companies
@@ -21,6 +22,7 @@ app.register_blueprint(hasher)
 app.register_blueprint(token)
 app.register_blueprint(accounts)
 app.register_blueprint(companies)
+app.register_blueprint(positions)
 app.register_blueprint(individuals)
 app.register_blueprint(authentication)
 app.register_blueprint(parent_companies)
---
+++ app/routes/positions.py
@@ -0,0 +1,80 @@
+from typing import Dict
+from http import HTTPStatus
+from flask import Blueprint, Response, jsonify, request
+from app.shared.cache import cache_response, delete_cache, get_cache
+
+
+positions = Blueprint("positions", __name__, url_prefix="/api/v1")
+
+
+@positions.get("/positions")
+def get_positions():
+    from app.queries.get_positions import get_positions
+
+    cached = get_cache("/positions")
+    match cached:
+        # CACHE HIT
+        case list() | dict():
+            return jsonify(cached)
+
+        # CACHE MISS
+        case None:
+            positions = get_positions()
+            match positions:
+                case list():
+                    cache_response("/positions", positions)
+                    return jsonify(positions)
+
+                case None:
+                    return Response(status=HTTPStatus.NOT_FOUND)
+
+
+@positions.post("/positions")
+def create_position():
+    from app.queries.create_position import create_position
+
+    payload: Dict[str, str] | None = request.json
+    match payload:
+        case dict():
+            position = payload.get("position")
+            match position:
+                case str():
+                    match create_position(position):
+                        case "Success":
+                            delete_cache("/positions")
+                            return Response(status=HTTPStatus.CREATED)
+
+                        case "NotUnique":
+                            return Response(status=HTTPStatus.CONFLICT)
+
+                        case "DatabaseUnavailable":
+                            return Response(status=HTTPStatus.SERVICE_UNAVAILABLE)
+
+                case None:
+                    return Response(status=HTTPStatus.BAD_REQUEST)
+        case None:
+            return Response(status=HTTPStatus.BAD_REQUEST)
+
+
+@positions.delete("/positions")
+def delete_position():
+    from app.queries.delete_position import delete_position
+
+    payload: Dict[str, str] | None = request.json
+    match payload:
+        case dict():
+            position = payload.get("position")
+            match position:
+                case str():
+                    match delete_position(position):
+                        case True:
+                            delete_cache("/positions")
+                            return Response(status=HTTPStatus.NO_CONTENT)
+
+                        case False:
+                            return Response(status=HTTPStatus.NOT_FOUND)
+
+                case None:
+                    return Response(status=HTTPStatus.BAD_REQUEST)
+        case None:
+            return Response(status=HTTPStatus.BAD_REQUEST)
