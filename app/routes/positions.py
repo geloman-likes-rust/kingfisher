aab7808f7535cf9af4060481f202e2f99c42945c
geloman <geloman-likes-rust@users.noreply.github.com>
2024-06-03 06:41:42 +0800

    Protect /positions endpoint by adding decorator @jwt_required

--- app/routes/positions.py
+++ app/routes/positions.py
@@ -2,6 +2,7 @@ from typing import Dict
 from http import HTTPStatus
 from flask import Blueprint, Response, jsonify, request
 from app.shared.cache import cache_response, delete_cache, get_cache
+from app.decorators.authorization import jwt_required, admin_required
 from app.shared.limiter import limiter
 
 
@@ -9,7 +10,8 @@ positions = Blueprint("positions", __name__, url_prefix="/api/v1")
 
 
 @positions.get("/positions")
-def get_positions():
+@jwt_required
+def get_positions(_):
     from app.queries.get_positions import get_positions
 
     cached = get_cache("/positions")
@@ -32,6 +34,8 @@ def get_positions():
 
 @positions.post("/positions")
 @limiter.limit("50/day;25/hour")
+@jwt_required
+@admin_required
 def create_position():
     from app.queries.create_position import create_position
 
@@ -59,6 +63,8 @@ def create_position():
 
 
 @positions.delete("/positions")
+@jwt_required
+@admin_required
 def delete_position():
     from app.queries.delete_position import delete_position
 
